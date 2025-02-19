9f41dd7aefc928a13686b3fb7cac05332f9ed737
Özcan Sert <ozcsert0@gmail.com>
2025-02-19 21:43:19 +0300

    [FE-1] - added: reach boilerplate

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Example } from "@/Components/Example/Example";
+
+const App = () => {
+  return (
+    <>
+      <Example sectionName="Example Section" />
+      <Example sectionName="Feature Section" />
+    </>
+  );
+};
+
+export default App;
