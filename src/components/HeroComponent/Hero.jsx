df07e0a6d7c8173e7107675800fa58707f61583a
nursahtuncel <nursahtuncell@gmail.com>
2025-03-19 09:02:26 +0300

    [FE-69] - Hero section completed.

---
+++ src/components/HeroComponent/Hero.jsx
@@ -0,0 +1,22 @@
+import styles from "./Hero.module.scss";
+import image from "../../assets/Hero/heroBackground.svg";
+
+function Hero() {
+  return (
+    <div className={styles.heroContainer}>
+      <img src={image} alt="" />
+      <div className={styles.heroContent}>
+        <h5>New Arrival</h5>
+        <h1>Discover Our New Collection</h1>
+        <p>
+          Explore Furniro's latest collection, where elegance meets innovation. Elevate
+          your space with our brand-new, stylish furniture pieces. Redefine your home with
+          the latest trends and timeless classics. Shop now!.
+        </p>
+        <button className={styles.heroBtn}>BUY Now</button>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,11 @@
 import { Example } from "@/components/Example/Example";
 import "./style.scss";
+import Hero from "../../components/HeroComponent/Hero";
 
 const HomePage = () => {
   return (
     <div>
+      <Hero sectionName="Hero Section" />
       <Example sectionName="Example Section" />
       <Example sectionName="Feature Section" />
     </div>
