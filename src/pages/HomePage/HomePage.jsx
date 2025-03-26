ed5d931a9b4997ecb96bdef0ed4f164c70866b37
nursahtuncel <nursahtuncell@gmail.com>
2025-03-27 00:04:04 +0300

    [FE-17] - fixed

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,14 +1,17 @@
+
 import "./style.scss";
 import { ExploreMoreSection } from "@/components/ExploreMoreSection/ExploreMoreSection";
 import { TopCategoriesSection } from "@/Components/TopCategoriesSection/TopCategories";
 import Hero from "../../components/HeroComponent/Hero";
 
+
 const HomePage = () => {
   return (
     <div>
+     
+      <Hero />
       <TopCategoriesSection />
       <ExploreMoreSection />
-      <Hero />
     </div>
   );
 };
