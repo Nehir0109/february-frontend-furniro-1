dd05cfedb0439e10c341e40a38bb2866126ea14f
nursahtuncel <nursahtuncell@gmail.com>
2025-03-26 21:41:33 +0300

    [FE-17] - homepage düzenlendi

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,13 @@
 import { ExploreMoreSection } from "@/components/ExploreMoreSection/ExploreMoreSection";
 import "./style.scss";
+import { TopCategoriesSection } from "@/Components/TopCategoriesSection/TopCategories";
+import Hero from "../../components/HeroComponent/Hero";
 
 const HomePage = () => {
   return (
     <div>
+      <Hero />
+      <TopCategoriesSection />
       <ExploreMoreSection sectionName=" ExploreMoreSection" />
     </div>
   );
