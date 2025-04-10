1c0ad731bf35537ae94d58a4a6b91f757737c568
nursahtuncel <nursahtuncell@gmail.com>
2025-04-10 23:47:01 +0300

    [FE-3] - navbar güncellendi

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,7 +1,7 @@
-import { Example } from "@/components/Example/Example";
 import { Outlet } from "react-router-dom";
 import "./layout.scss";
 import Navbar from "./Components/NavbarComponent/Navbar.jsx";
+import Footer from "./Components/Footer/Footer.jsx";
 
 export const Layout = () => {
   return (
