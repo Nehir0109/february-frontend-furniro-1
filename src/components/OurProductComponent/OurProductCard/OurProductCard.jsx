28be18ab0004ea1509ed8029dfff5f3953cefaff
talshelin <talshicranhelin@gmail.com>
2025-04-12 16:16:21 +0300

    [FE-10] - card details added

--- src/components/OurProductComponent/OurProductCard/OurProductCard.jsx
+++ src/components/OurProductComponent/OurProductCard/OurProductCard.jsx
@@ -1,11 +1,30 @@
 import style from "./OurProductCard.module.scss";
 import { StarRating } from "@/Components/StarRating/StarRating";
+import { FaHeart, FaSearch, FaShoppingCart } from "react-icons/fa";
+import { useState } from "react";
 
 export const OurProductCard = ({ product }) => {
+  const [isFavorite, setIsFavorite] = useState(false);
+
+  const toggleFavorite = () => {
+    setIsFavorite((prev) => !prev);
+  };
+
   return (
     <div className={style.cardWrapper}>
       <div className={style.cardImageDiv}>
         <img src={product.images} alt={product.name} className={style.cardImage} />
+        <div className={style.hoverActions}>
+          <button className={style.iconButton} onClick={toggleFavorite}>
+            <FaHeart className={isFavorite ? style.filledHeart : ""} />
+          </button>
+          <button className={style.iconButton}>
+            <FaSearch />
+          </button>
+          <button className={style.iconButton}>
+            <FaShoppingCart />
+          </button>
+        </div>
       </div>
       <div className={style.cardDetails}>
         <h3 className={style.cardTitle}>{product.name}</h3>
@@ -16,7 +35,7 @@ export const OurProductCard = ({ product }) => {
         <div className={style.cardPrice}>
           <span className={style.currentPrice}>${product.price}</span>
           {product.originalPrice && (
-            <span className={style.oldPrice}>${product.originalPrice}</span>
+            <span className={style.price}>${product.originalPrice}</span>
           )}
         </div>
       </div>
