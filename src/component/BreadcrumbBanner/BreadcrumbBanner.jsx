71d8762bd888eaa5c34c819991136718706ac693
nursahtuncel <nursahtuncell@gmail.com>
2025-04-21 20:43:13 +0300

    [FE-43] - kodlar sadeleştirdi

--- src/component/BreadcrumbBanner/BreadcrumbBanner.jsx
+++ src/component/BreadcrumbBanner/BreadcrumbBanner.jsx
@@ -1,20 +1,20 @@
 import { Link, useLocation } from "react-router-dom";
-import arrow from "../../assets/ShopPage/arrow.svg";
+import arrow from "@/assets/ShopPage/arrow.svg";
 import style from "./BreadcrumbBanner.module.scss";
-import capitalizeFirstLetter from "../../Utils/capitalizeFirstLetter";
+import capitalizeFirstLetter from "@/Utils/capitalizeFirstLetter";
 const BreadCrumbBanner = () => {
   const location = useLocation();
-  const pathname = location.pathname.split("/").filter((x) => x);
+  const pathnames = location.pathname.split("/").filter((path) => path);
 
   return (
     <nav>
       <ul className={style.breadcrumb}>
-        <li className={`${style.tohomeItem} ${style.breadcrumbItem}`}>
+        <li className={style.breadcrumbItem}>
           <Link to="/">Home</Link>
         </li>
         <img src={arrow} alt="" className={style.arrow} />
         <li className={style.breadcrumbItem}>
-          <span>{capitalizeFirstLetter(pathname[0])}</span>
+          <span>{capitalizeFirstLetter(pathnames[0])}</span>
         </li>
       </ul>
     </nav>
