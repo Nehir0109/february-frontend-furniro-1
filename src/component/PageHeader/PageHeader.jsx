e9a7d7fe2e5aa02cb4f290bc50f654ba0c6e7f18
nursahtuncel <nursahtuncell@gmail.com>
2025-04-21 15:32:41 +0300

    [FE-43] - pageheader html css yapısı oluşturuldu.

--- src/component/PageHeader/PageHeader.jsx
+++ src/component/PageHeader/PageHeader.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import style from "./Pageheader.module.scss";
+import Logo from "../../assets/ShopPage/logo.svg";
+import { useLocation } from "react-router-dom";
+import BreadcrumbBanner from "../BreadcrumbBanner/BreadcrumbBanner";
 
 const PageHeader = () => {
-  return <div>PageHeader</div>;
+  const location = useLocation();
+  console.log(location.pathname);
+  return (
+    <div className={style.headerComponent}>
+      <div className={style.headerBackground}></div>
+      <div className={style.headerContent}>
+        <img src={Logo} alt="logo" />
+        <h1> {location.pathname}</h1>
+        <BreadcrumbBanner />
+      </div>
+    </div>
+  );
 };
 
 export default PageHeader;
