b24edc935e9840ca332e10a24b608a100fcd373e
nursahtuncel <nursahtuncell@gmail.com>
2025-04-21 20:38:38 +0300

    [FE-43] - Breadcrumb navigasyonları eklendi

--- src/component/BreadcrumbBanner/BreadcrumbBanner.jsx
+++ src/component/BreadcrumbBanner/BreadcrumbBanner.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import { Link, useLocation } from "react-router-dom";
+import arrow from "../../assets/ShopPage/arrow.svg";
+import style from "./BreadcrumbBanner.module.scss";
+import capitalizeFirstLetter from "../../Utils/capitalizeFirstLetter";
+const BreadCrumbBanner = () => {
+  const location = useLocation();
+  const pathname = location.pathname.split("/").filter((x) => x);
 
-const BreadcrumbBanner = () => {
-  return <div>BreadcrumbBanner</div>;
+  return (
+    <nav>
+      <ul className={style.breadcrumb}>
+        <li className={`${style.tohomeItem} ${style.breadcrumbItem}`}>
+          <Link to="/">Home</Link>
+        </li>
+        <img src={arrow} alt="" className={style.arrow} />
+        <li className={style.breadcrumbItem}>
+          <span>{capitalizeFirstLetter(pathname[0])}</span>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
-export default BreadcrumbBanner;
+export default BreadCrumbBanner;
