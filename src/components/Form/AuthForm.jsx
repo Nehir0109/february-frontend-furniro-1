f64f8f5f67abfaf1b8ae7a19f6645de190fe9905
Nehir0109 <nehir.cakilci@gmail.com>
2025-04-30 19:55:21 +0300

    [FE-21] - signup and login page created

--- src/components/Form/AuthForm.jsx
+++ src/components/Form/AuthForm.jsx
@@ -1,90 +1,92 @@
+import { useSearchParams } from "react-router-dom";
 import style from "./AuthForm.module.scss";
 import React, { useState } from 'react';
-import { useHistory } from 'react-router-dom';
 
-const AuthForm = ({ type }) => {
+const AuthForm = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [fullName, setFullName] = useState(''); // Sadece signup için gerekli
-    const history = useHistory();
+    const [searchParams] = useSearchParams();
+    const type = searchParams.get("type");
 
     // Form submission process
     const handleSubmit = (e) => {
         e.preventDefault();
-        
-        if(type === 'login' ){
+
+        if (type === 'login') {
             console.log('Login with:', { email, password });
         } else if (type === 'signup') {
             console.log('Sign up with:', { fullName, email, password });
         }
     };
 
-    const handleLoginRedirect = () => {
-        history.push('/login');
-    };
-
     return (
-        <form onSubmit={handleSubmit}>
-            <h2>{type === 'login' ? 'Login' : 'Create your Free Account'}</h2>
+        <form className={style.form} onSubmit={handleSubmit}>
+            <h2 className={style.title} >{type === 'login' ? 'Login' : 'Create your Free Account'}</h2>
 
-            {type === 'signup' && (
-                <div>
-                    <label htmlFor="fullName">Full name</label>
-                    <input 
-                       type="text"
-                       id="fullname"
-                       placeholder="John Doe"
-                       value={fullName}
-                       onChange={(e) => setFullName(e.target.value)}
-                    />
-                </div> 
-            )}
-            <div>
-                <label htmlFor="email">Email address</label>
-                <input 
-                   type="email"
-                   id="email"
-                   placeholder="Abc@def.com"
-                   value={email}
-                   onChange={(e) => setEmail(e.target.value)}
-                   required
-                />
-            </div>
-            <div>
-            <label htmlFor="password">Password</label>
-                <input
-                    type="password"
-                    id="password"
-                    placeholder="Password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    required
-                />
-            </div>
-
-            <div className="social-buttons">
-                <p>- OR -</p>
-                <div className="social-button-container">
-                    <button type="button">
-                        <img src="./src/assets/AuthPage/github.svg" alt="GitHub Logo"/>
-                        Sign up with GitHub
-                    </button>
-                    <button type="button">
-                        <img src="./src/assets/AuthPage/google.svg" alt="Google Logo"/>
-                        Sign up with Google
-                    </button>
-                </div>
-                
+            <div className={style.formContainer}>
                 {type === 'signup' && (
-                    <div>
-                        <p>Already have an account? <button type="button" onClick={handleLoginRedirect}>Log in</button></p>
+                    <div className={style.inputContent}>
+                        <label className={style.label} htmlFor="fullName">Full name</label>
+                        <input
+                            className={style.input}
+                            type="text"
+                            id="fullname"
+                            placeholder="John Doe"
+                            value={fullName}
+                            onChange={(e) => setFullName(e.target.value)}
+                        />
                     </div>
                 )}
-            </div>
+                <div className={style.inputContent}>
+                    <label className={style.label} htmlFor="email">Email address</label>
+                    <input
+                        className={style.input}
+                        type="email"
+                        id="email"
+                        placeholder="Abc@def.com"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                    />
+                </div>
+                <div className={style.inputContent}>
+                    <label className={style.label} htmlFor="password">Password</label>
+                    <input
+                        className={style.input}
+                        type="password"
+                        id="password"
+                        placeholder="Password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                    />
+                </div>
 
-            <button type="submit">
-                {type === 'login' ? 'Login' : 'Create Acount'}
-            </button>
+                <div className={style.socialButtonContainer}>
+                    <p className={style.text}>- OR -</p>
+                    <div className={style.socialButtonContent}>
+                        <button className={style.socialButton} type="button">
+                            <img src="./src/assets/AuthPage/github.svg" alt="GitHub Logo" />
+                            Sign up with GitHub
+                        </button>
+                        <button className={style.socialButton} type="button">
+                            <img src="./src/assets/AuthPage/google.svg" alt="Google Logo" />
+                            Sign up with Google
+                        </button>
+                    </div>
+
+                    {type === 'signup' && (
+                        <div className={style.signupTextContainer}>
+                            <p className={style.signupText}>Already have an account? <button className={style.signupButton} type="button">Log in</button></p>
+                        </div>
+                    )}
+
+                    <button className={style.submitButton} type="submit">
+                        {type === 'login' ? 'Login' : 'Create Acount'}
+                    </button>
+                </div>
+            </div>
         </form>
     );
 }
