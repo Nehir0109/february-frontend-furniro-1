1db6ebc057ff6b8ca100924fc1d9e002513e54ca
seymagunay <161377330+seymagunay@users.noreply.github.com>
2025-04-30 20:07:21 +0300

    [FE-99] - Feature Section

--- src/Components/FeatureSection/FeatureCard/FeatureCard.jsx
+++ src/Components/FeatureSection/FeatureCard/FeatureCard.jsx
@@ -1,10 +1,17 @@
-export const FeatureCard = (featureCard) =>{
-    return (
-        <div ClassName = "featureCards" >
-        <img src="{featureCard.img }" alt="" />
-        <div ClassName = "connet">
-        <h3>{featureCard.title}</h3>
-        <p>{featureCard.name}</p>
-        </div>
-        </div>)
-    };
+/* eslint-disable react/prop-types */
+
+import style from "./FeatureCard.module.scss";
+
+const FeatureCard = ({ featureCard }) => {
+return (
+<div className={style.featureCard}>
+<img src={featureCard.img} alt={featureCard.title} />
+<div className={style.content}>
+<h3>{featureCard.title}</h3>
+<p>{featureCard.name}</p>
+</div>
+</div>
+);
+};
+
+export default FeatureCard;
--- src/Components/FeatureSection/FeatureSection.jsx
+++ src/Components/FeatureSection/FeatureSection.jsx
@@ -1,38 +1,37 @@
-import style from "./FeatureSection.module.scss"
-import { FeatureCard } from "@./FeatureCard/FeatureCard"
-// rafce kısayol tuşu
+import style from "./FeatureSection.module.scss";
+import FeatureCard from "./FeatureCard/FeatureCard";
 
 const featureCards = [
-  {
-   img:"src/assets/FeatureCard/trophy.png",
-   title: "High Quality",
-   name : "crafted from top materials"
-  },
-  {
-    img:"src/assets/FeatureCard/guarantee.png",
-    title: "Warranty Protection",
-    name : "Over 2 years"
-   },
-   {
-    img:"src/assets/FeatureCard/shipping.png",
-    title: "Free Shipping",
-    name : "Order over 150 $"
-   },
-   {
-    img:"src/assets/FeatureCard/customer-support.png",
-    title: "24 / 7 Support",
-    name : "Dedicated support"
-   }
-]
+{
+img: "src/assets/FeatureCard/trophy.png",
+title: "High Quality",
+name: "crafted from top materials",
+},
+{
+img: "src/assets/FeatureCard/guarantee.png",
+title: "Warranty Protection",
+name: "Over 2 years",
+},
+{
+img: "src/assets/FeatureCard/shipping.png",
+title: "Free Shipping",
+name: "Order over 150 $",
+},
+{
+img: "src/assets/FeatureCard/customer-support.png",
+title: "24 / 7 Support",
+name: "Dedicated support",
+},
+];
 
-export const FeatureSection = ( ) =>{
-  return (
-    <div className="cards">
-    {featureCards.map((feature,index)=>{
-      <FeatureCard key={index} {...feature} />
-    })}
-    </div>)
-  };
+export const FeatureSection = () => {
+return (
+<div className={style.cards}>
+{featureCards.map((feature, index) => (
+<FeatureCard key={index} featureCard={feature} />
+))}
+</div>
+);
+};
 
-
-export default FeatureSection
+export default FeatureSection;
